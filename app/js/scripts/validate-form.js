7c7dea8b4854492397e537f10c0660c0a0d1f2c8
Al Zaplin <zaplin.dev@gmail.com>
2025-12-12 18:32:10 +0300

    Feat: (form validation & modal info window) Check if the email address is valid. If so, change the disabled form button. Also, show a modal window to inform the user after the form is sent

--- app/js/scripts/validate-form.js
+++ app/js/scripts/validate-form.js
@@ -1,3 +1,44 @@
 export default function validateForm() {
-  const input = document.querySelector;
+  const form = document.querySelector(".subscribe__form");
+  const input = form.querySelector(".subscribe__form-input");
+  const button = form.querySelector(".subscribe__button");
+
+  const modal = document.querySelector(".modal");
+  const modalButton = modal.querySelector(".modal__button");
+
+  const scrollbarWidth =
+    window.innerWidth - document.documentElement.clientWidth;
+
+  const regEx = /^[a-zA-Z0–9._%+-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,}$/;
+
+  const validateEmail = (input) => regEx.test(input);
+  const cleanForm = () => {
+    input.value = "";
+    form.classList.remove("enabled");
+    button.disabled = true;
+  };
+
+  input.addEventListener("input", (evt) => {
+    const inputValue = evt.currentTarget.value;
+
+    if (!validateEmail(inputValue)) {
+      form.classList.remove("enabled");
+      button.disabled = true;
+    } else {
+      form.classList.add("enabled");
+      button.disabled = false;
+    }
+  });
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    document.body.style.cssText = `overflow: hidden; padding-right: ${scrollbarWidth}px;`;
+    modal.classList.add("visible");
+  });
+
+  modalButton.addEventListener("click", () => {
+    cleanForm();
+    modal.classList.remove("visible");
+    setTimeout(() => (document.body.style.cssText = `overflow: visible;`), 400);
+  });
 }
